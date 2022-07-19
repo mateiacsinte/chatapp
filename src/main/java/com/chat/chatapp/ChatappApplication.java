54136028a7087edf95278e9345ee5a373731fa3e
mateiacsinte <matei.acsinte@mincode.ro>
2022-07-19 17:07:51 +0300

    Added the springboot template for the chat app

---
+++ src/main/java/com/chat/chatapp/ChatappApplication.java
@@ -0,0 +1,16 @@
+package com.chat.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class ChatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/chat/chatapp/dto/UserDTO.java
@@ -0,0 +1,5 @@
+package com.chat.chatapp.dto;
+
+public class UserDTO {
+
+}
